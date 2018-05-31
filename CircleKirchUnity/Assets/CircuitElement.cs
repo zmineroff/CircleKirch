75eea51133ed066ab7955a244369d02b85a41543
Zach Mineroff <zmineroff@gmail.com>
2018-05-31 12:39:56 -0400

    Make battery prefab

---
+++ CircleKirchUnity/Assets/CircuitElement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircuitElement : MonoBehaviour {
+
+	public bool Voltage;
+	public bool Current;
+	public bool Resistance;
+
+	public List<Terminal> terminals = new List<Terminal> ();
+
+	// Use this for initialization
+	void Start () {
+		terminals.AddRange(GetComponentsInChildren<Terminal> ());
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ CircleKirchUnity/Assets/Wire.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wire : MonoBehaviour {
+
+	public Terminal terminalA;
+	public Terminal terminalB;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//where drawing happens
+		Debug.DrawLine(terminalA.transform.position, terminalB.transform.position);
+	}
+}
