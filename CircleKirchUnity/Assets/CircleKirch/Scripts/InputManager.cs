5eb7da9525dea49b3e694d96d2bac526186ddc10
Zach Mineroff <zmineroff@gmail.com>
2018-07-04 14:37:14 -0400

    disable drag and drop elements in play mode

--- CircleKirchUnity/Assets/CircleKirch/Scripts/InputManager.cs
+++ CircleKirchUnity/Assets/CircleKirch/Scripts/InputManager.cs
@@ -7,6 +7,8 @@ public class InputManager : MonoBehaviour {
 	public enum Mode {LevelEditor, Play}
 	public Mode mode = Mode.LevelEditor;
 
+	public GameObject levelEditorCanvas;
+	public GameObject playCanvas;
 
 	// Use this for initialization
 	void Start () {
@@ -21,11 +23,26 @@ public class InputManager : MonoBehaviour {
 			Debug.Log("Edit mode");
 			this.GetComponent<InputPlay>().enabled = false;
 			this.GetComponent<InputLevelEditor>().enabled = true;
+
+			levelEditorCanvas.SetActive(true);
+
+			var circuitElements = FindObjectsOfType<CircuitElement>();
+			foreach (CircuitElement circuitElement in circuitElements) {
+				circuitElement.GetComponent<ItemDragDrop>().enabled=true;
+			}
+
 		} else if (Input.GetKeyDown(KeyCode.P)) {
 			mode = Mode.Play;
 			Debug.Log("Play mode");
 			this.GetComponent<InputPlay>().enabled = true;
 			this.GetComponent<InputLevelEditor>().enabled = false;
+			
+			levelEditorCanvas.SetActive(false);
+			
+			var circuitElements = FindObjectsOfType<CircuitElement>();
+			foreach (CircuitElement circuitElement in circuitElements) {
+				circuitElement.GetComponent<ItemDragDrop>().enabled=false;
+			}
 		}
 	}
 }
--- CircleKirchUnity/Assets/CircleKirch/Scripts/ItemDragDrop.cs
+++ CircleKirchUnity/Assets/CircleKirch/Scripts/ItemDragDrop.cs
@@ -7,12 +7,16 @@ public class ItemDragDrop : MonoBehaviour {
     private Vector3 offset;
 
     void OnMouseDown() {
-        offset = gameObject.transform.position -
-            Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+        if (enabled) {
+            offset = gameObject.transform.position -
+                Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+        }
     }
 
     void OnMouseDrag() {
-        Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
-        transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
+        if (enabled) {
+            Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
+            transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
+        }
     }
 }
